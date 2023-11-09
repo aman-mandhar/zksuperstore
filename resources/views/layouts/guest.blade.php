482092a1e4477a6f3e2e6c1cd89c46becf8dfcd2
aman-mandhar <parthmultisolution@gmail.com>
2023-11-09 20:58:38 +0530

    zkuperstore

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+    
+    <!-- Style -->
+    
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+        
+    @livewireStyles  
+</head>
+<body>
+    @include('layouts.inc.user.navbar')
+
+         <main class="py-4">
+            @yield('content')
+        </main>
+    @include('layouts.inc.user.footer')
+    
+
+<!-- Scripts -->
+<script src="{{ asset('assets/js/bootstrap.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/jquery-3.7.1.min.js') }}"></script>
+@livewireScripts
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,33 @@
-@extends('layouts.app')
-
+@extends('layouts.guest')
 @section('content')
-        <div class="container-fluid page-body-wrapper">
-        <h1> hello</h1>
+        
+            <section class="hero-section"><img src="{{ asset('assets/user/images/hero.png') }}" alt="card"></section>
+            <section class="shop-section">
+              <div class="shop-images">
+                <div class="shop-link">
+                  <h3>Shop Laptops &amp; Tables</h3>
+                  <img src="{{ asset('assets/user/images/img-1.png') }}" alt="card">
+                  <a href="#">Shop now</a>
+                </div>
+                <div class="shop-link">
+                  <h3>Shop Smartwatches</h3>
+                  <img src="{{ asset('assets/user/images/img-2.png') }}" alt="card">
+                  <a href="#">Shop now</a>
+                </div>
+                <div class="shop-link">
+                  <h3>Create with Strip Lights</h3>
+                  <img src="{{ asset('assets/user/images/img-3.png') }}" alt="card">
+                  <a href="#">Shop now</a>
+                </div>
+                <div class="shop-link">
+                  <h3>Home Refresh Ideas</h3>
+                  <img src="{{ asset('assets/user/images/img-4.png') }}" alt="card">
+                  <a href="#">Shop now</a>
+                </div>
+              </div>
+            </section>
 
-        </div>
-        @livewireScripts
+         @livewireScripts
     </body>
     
 </html>
