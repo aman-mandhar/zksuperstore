73e4350cbf12a2c3ad66b0e5a1528939a25281ec
aman-mandhar <parthmultisolution@gmail.com>
2023-12-24 13:00:52 +0530

    logout

--- resources/views/layouts/inc/user/navbar.blade.php
+++ resources/views/layouts/inc/user/navbar.blade.php
@@ -32,23 +32,14 @@
 
   <div class="sign-in">
    @guest
-@if (Route::has('login'))
-  <li class="nav-item">
-      <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-  </li>
-@endif
-
-@if (Route::has('register'))
-  
-  <li>
+   @if (Route::has('register'))
       <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-  </li>
-@endif
-@else
-<li class="nav-item dropdown">
-  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-    <p>Hello, {{ Auth::user()->name }}</p>
-  </a>
+   @endif
+   @else
+      <a href="#" role="button">
+      <p>Hello, {{ Auth::user()->name }}</p>
+      </a>
+  </div>
 
   <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
       <a class="dropdown-item" href="{{ route('logout') }}"
@@ -61,7 +52,7 @@
           @csrf
       </form>
   </div>
-</li>
+
 @endguest
   </div>
 
